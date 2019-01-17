c15762d592a4b85f5ae4f337f48b07160e52f163
zhaojinhao <2481406659@qq.com>
2019-01-17 19:29:00 +0800

    删除最后一个敏感字符、显示所有车位坐标

--- main.c
+++ main.c
@@ -27,23 +27,48 @@ int main()
             printf("aaa\n");
             int x;
             int y;
-            scanf("%d",&x);
-            scanf("%d",&y);
+            scanf("%d", &x);
+            scanf("%d", &y);
 
-            arr[index].x=x;
-            arr[index].y=y;
+            arr[index].x = x;
+            arr[index].y = y;
 
             index++;
             printf("aaa\n");
             char xx;
-            scanf("%c",&xx);
-            scanf("%c",&xx);
+            scanf("%c", &xx);
+            scanf("%c", &xx);
         }
         if (code == 2)
         {
+            printf("bbb\n");
+            if (index > 0)
+            {
+                index--;
+                printf("bbb\n");
+                char xx;
+                scanf("%c", &xx);
+                scanf("%c", &xx);
+            }
+            else
+            {
+                printf("bbb\n");
+                char xx;
+                scanf("%c", &xx);
+                scanf("%c", &xx);
+            }
         }
         if (code == 3)
         {
+            printf("ccc\n");
+            for (int i = 0; i < index; i++)
+            {
+                printf("%d (%d %d)\n", i + 1, arr[i].x, arr[i].y);
+            }
+            printf("ccc\n");
+            char xx;
+            scanf("%c", &xx);
+            scanf("%c", &xx);
         }
         if (code == 4)
         {
