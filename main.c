32cde89ec0868ca636b69867c2c41ab112fb5c05
zhaojinhao <2481406659@qq.com>
2019-01-17 19:18:18 +0800

    结构框架

--- main.c
+++ main.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
-
+struct Point{
+    int x;
+    int y;
+} ;
 int main(){
+    int index=0;
+    struct Point arr[100];
+    printf("huanyingjinrutingchexitong\n");
 
-    printf("hello world!");
+    while(1){
+        printf("1---\n");
+        printf("2---\n");
+        printf("3---\n");
+        printf("4---\n");
+        printf("5---\n");
+
+        printf("qingxuanzexiangyinggongneng\n");
+        int code;
+        scanf("%d",&code);
+
+        if(code == 1){
+
+        }
+        if(code == 2){
+            
+        }
+        if(code == 3){
+            
+        }
+        if(code == 4){
+            
+        }
+        if(code == 5){
+            printf("fff\n");
+            break;
+            
+        }
+    }
 
     return 0;
 
