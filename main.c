4815c7112b6b53ee55efc57a93a090e059d54077
zhaojinhao <2481406659@qq.com>
2019-01-17 19:22:32 +0800

    增加车辆位置

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-struct Point{
+struct Point
+{
     int x;
     int y;
-} ;
-int main(){
-    int index=0;
+};
+int main()
+{
+    int index = 0;
     struct Point arr[100];
     printf("huanyingjinrutingchexitong\n");
 
-    while(1){
+    while (1)
+    {
         printf("1---\n");
         printf("2---\n");
         printf("3---\n");
@@ -17,27 +20,40 @@ int main(){
 
         printf("qingxuanzexiangyinggongneng\n");
         int code;
-        scanf("%d",&code);
+        scanf("%d", &code);
 
-        if(code == 1){
+        if (code == 1)
+        {
+            printf("aaa\n");
+            int x;
+            int y;
+            scanf("%d",&x);
+            scanf("%d",&y);
 
+            arr[index].x=x;
+            arr[index].y=y;
+
+            index++;
+            printf("aaa\n");
+            char xx;
+            scanf("%c",&xx);
+            scanf("%c",&xx);
         }
-        if(code == 2){
-            
+        if (code == 2)
+        {
         }
-        if(code == 3){
-            
+        if (code == 3)
+        {
         }
-        if(code == 4){
-            
+        if (code == 4)
+        {
         }
-        if(code == 5){
+        if (code == 5)
+        {
             printf("fff\n");
             break;
-            
         }
     }
 
     return 0;
-
 }
